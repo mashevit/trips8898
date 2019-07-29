5afbe0a4be0baf12b62c50b63f37a76ae9a57c07
mashevit <40078104+mashevit@users.noreply.github.com>
2019-07-29 17:52:07 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 REPLACE INTO `role` VALUES (1,'ADMIN');
 REPLACE INTO `role` VALUES (2,'USER');
 REPLACE INTO `role` VALUES (3,'USER2');
-REPLACE INTO `Taveler` VALUES (1,'1986-01-08','Ilia Mahsevitzky');
+REPLACE INTO `Taveler` VALUES (3,'1986-01-08','Ilia Mahsevitzky');
 REPLACE INTO `Taveler` VALUES (2,'1985-08-08','Trav Aller');
